11f98165e8e1d07d7d0f2f10a41e90b5ae3a0c78
Ben McMahan <benjamin@linkedresponder.com>
2026-06-22 19:08:39 -0400

    Rebuild v0.1.4 APK with current stamp

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.4",
-  "commit": "4f8fda5a",
-  "commitMessage": "Rebuild v0.1.3 APK with current stamp",
-  "builtAt": "2026-06-22T23:07:08.621Z"
+  "commit": "07b22de9",
+  "commitMessage": "Add EMAY pulse oximetry import",
+  "builtAt": "2026-06-22T23:08:06.713Z"
 };
