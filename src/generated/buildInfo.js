4361353cfdf572c5b56002cb5d9cea3906358504
Ben McMahan <benjamin@linkedresponder.com>
2026-06-22 19:12:56 -0400

    Rebuild v0.1.5 APK with current stamp

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.5",
-  "commit": "11f98165",
-  "commitMessage": "Rebuild v0.1.4 APK with current stamp",
-  "builtAt": "2026-06-22T23:11:41.280Z"
+  "commit": "3ef0bb75",
+  "commitMessage": "Align pulse ox CSV to session timeline",
+  "builtAt": "2026-06-22T23:12:25.625Z"
 };
