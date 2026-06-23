6c83c6f4b190c3aad6201e9e992e15344800525e
Ben McMahan <benjamin@linkedresponder.com>
2026-06-22 20:39:32 -0400

    Rebuild v0.1.6 APK with current stamp

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.6",
-  "commit": "4361353c",
-  "commitMessage": "Rebuild v0.1.5 APK with current stamp",
-  "builtAt": "2026-06-23T00:38:15.338Z"
+  "commit": "7d638597",
+  "commitMessage": "Fix session detail loading guard",
+  "builtAt": "2026-06-23T00:38:59.593Z"
 };
