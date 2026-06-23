e05ea57985fc77a93939b7ff31848ceb90bf2937
Ben McMahan <benjamin@linkedresponder.com>
2026-06-23 18:00:40 -0400

    Refresh build info for profiler button APK

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.16",
-  "commit": "9f5b93a",
-  "commitMessage": "Refresh build info for profile APK build",
-  "builtAt": "2026-06-23T21:59:14.192Z"
+  "commit": "775b9b0",
+  "commitMessage": "Clarify profiler generate button wait states",
+  "builtAt": "2026-06-23T21:59:55.934Z"
 };
