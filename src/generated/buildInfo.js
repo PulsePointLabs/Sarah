a62cc21a808b951c7f33d5f96cf77532cc1ee218
Ben McMahan <benjamin@linkedresponder.com>
2026-06-22 21:12:14 -0400

    Bump Sarah to v0.1.7 mobile video build

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.6",
-  "commit": "7d638597",
-  "commitMessage": "Fix session detail loading guard",
-  "builtAt": "2026-06-23T00:38:59.593Z"
+  "version": "0.1.7",
+  "commit": "458b2942",
+  "commitMessage": "Add APK live session video recording flow",
+  "builtAt": "2026-06-23T01:10:35.420Z"
 };
