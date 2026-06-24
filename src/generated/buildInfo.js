7ba2e7266e074679327d740375d4e60c4d54543b
Ben McMahan <benjamin@linkedresponder.com>
2026-06-23 20:57:17 -0400

    Refresh build info for profiler status simplification

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.16",
-  "commit": "969dbf3",
-  "commitMessage": "Refresh build info for anatomy handoff",
-  "builtAt": "2026-06-24T00:46:05.150Z"
+  "commit": "da6d745",
+  "commitMessage": "Simplify profiler review run status",
+  "builtAt": "2026-06-24T00:56:56.360Z"
 };
