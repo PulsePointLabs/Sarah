f9d6e6fd9f3035c2172a4e01c858fb1fa5d39c1b
Ben McMahan <benjamin@linkedresponder.com>
2026-06-24 03:07:19 -0400

    Refresh build info for media telemetry rail fix

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.16",
-  "commit": "028312a",
-  "commitMessage": "Refresh build info for Howl sync desktop",
-  "builtAt": "2026-06-24T06:39:44.249Z"
+  "commit": "1d920ce",
+  "commitMessage": "Stabilize live media telemetry rail layout",
+  "builtAt": "2026-06-24T07:06:15.138Z"
 };
