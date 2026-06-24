840d2417a37774b790e53d47646b3148681a7bd8
Ben McMahan <benjamin@linkedresponder.com>
2026-06-23 20:46:54 -0400

    Refresh build info after artifact rebuild

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.16",
-  "commit": "e92fc92",
-  "commitMessage": "Clarify anatomy video background handoff",
-  "builtAt": "2026-06-24T00:42:03.730Z"
+  "commit": "969dbf3",
+  "commitMessage": "Refresh build info for anatomy handoff",
+  "builtAt": "2026-06-24T00:46:05.150Z"
 };
