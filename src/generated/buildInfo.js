caa1cf22d9532e59b199de0fc070e74b330550ab
Ben McMahan <benjamin@linkedresponder.com>
2026-06-23 21:43:13 -0400

    Refresh build info for anatomy timeout APK

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.16",
-  "commit": "7ba2e72",
-  "commitMessage": "Refresh build info for profiler status simplification",
-  "builtAt": "2026-06-24T00:58:36.169Z"
+  "commit": "c87ebb2",
+  "commitMessage": "Recover anatomy video jobs after request timeout",
+  "builtAt": "2026-06-24T01:41:58.791Z"
 };
