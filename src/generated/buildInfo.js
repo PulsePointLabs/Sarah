969dbf3827a5de9ce8b1b1b47daaae1eda8e7690
Ben McMahan <benjamin@linkedresponder.com>
2026-06-23 20:42:25 -0400

    Refresh build info for anatomy handoff

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.16",
-  "commit": "8b3cd25",
-  "commitMessage": "Diversify profiler anatomy evidence visuals",
-  "builtAt": "2026-06-24T00:12:46.264Z"
+  "commit": "e92fc92",
+  "commitMessage": "Clarify anatomy video background handoff",
+  "builtAt": "2026-06-24T00:42:03.730Z"
 };
