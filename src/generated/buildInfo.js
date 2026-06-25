d4ffa145873697c99c42a966d563b46e02dc61e1
Ben McMahan <benjamin@linkedresponder.com>
2026-06-25 19:34:52 -0400

    Refresh build info after desktop rebuild

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.29",
-  "commit": "4afc726",
-  "commitMessage": "Fix anatomy video evidence region selection",
-  "builtAt": "2026-06-25T23:24:13.452Z"
+  "commit": "fa10c16",
+  "commitMessage": "Allow head-to-toe multi-region anatomy evidence",
+  "builtAt": "2026-06-25T23:32:15.774Z"
 };
