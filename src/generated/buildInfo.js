068781ac3b324513776359eabf004e83f74355be
Ben McMahan <benjamin@linkedresponder.com>
2026-06-26 18:23:55 -0400

    Refresh Sarah Android build stamp

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.34",
-  "commit": "3404166",
-  "commitMessage": "Refresh build info after SarahVS merge",
-  "builtAt": "2026-06-26T22:19:58.862Z"
+  "commit": "b7b0eb9",
+  "commitMessage": "Refresh build info after desktop pack",
+  "builtAt": "2026-06-26T22:21:45.318Z"
 };
