b7b0eb99a50d07897d2e8c9557cf22471549225b
Ben McMahan <benjamin@linkedresponder.com>
2026-06-26 18:21:10 -0400

    Refresh build info after desktop pack

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.34",
-  "commit": "d63fd1b",
-  "commitMessage": "Accept full SarahVS session transfers",
-  "builtAt": "2026-06-26T22:18:47.881Z"
+  "commit": "3404166",
+  "commitMessage": "Refresh build info after SarahVS merge",
+  "builtAt": "2026-06-26T22:19:58.862Z"
 };
