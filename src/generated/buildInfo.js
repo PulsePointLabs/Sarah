fece9d6b26040ec28d97145eb7444c96b46e1458
Ben McMahan <benjamin@linkedresponder.com>
2026-06-26 23:27:20 -0400

    Refresh Sarah 0.1.41 build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.40",
-  "commit": "66b4914",
-  "commitMessage": "Restore complete anatomy evidence merge",
-  "builtAt": "2026-06-27T03:06:29.260Z"
+  "version": "0.1.41",
+  "commit": "6519bcb",
+  "commitMessage": "Fix mixed anatomy evidence and refusal reporting",
+  "builtAt": "2026-06-27T03:26:19.104Z"
 };
