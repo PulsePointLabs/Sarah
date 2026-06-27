d160e44ede3cf5191c5218a37f97e11ba902a986
Ben McMahan <benjamin@linkedresponder.com>
2026-06-26 22:04:58 -0400

    Refresh Sarah build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.39",
-  "commit": "ccb8369",
-  "commitMessage": "Move Vital Signs narration to session detail",
-  "builtAt": "2026-06-27T01:43:56.394Z"
+  "commit": "826f4b0",
+  "commitMessage": "Return background job IDs before rendering",
+  "builtAt": "2026-06-27T02:03:17.836Z"
 };
