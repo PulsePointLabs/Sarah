8bc4ff260d55d5f670b917428fd7c26a74bbdb9d
Ben McMahan <benjamin@linkedresponder.com>
2026-06-27 00:24:41 -0400

    Refresh Sarah 0.1.41 build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.41",
-  "commit": "ab2d764",
-  "commitMessage": "Avoid blocking profiler archive scans",
-  "builtAt": "2026-06-27T03:49:22.536Z"
+  "commit": "7369743",
+  "commitMessage": "Make profiler reviews longitudinal by default",
+  "builtAt": "2026-06-27T04:23:33.447Z"
 };
