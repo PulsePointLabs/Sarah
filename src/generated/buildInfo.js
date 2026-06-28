495cca9f880f84a33a8f455b6ee7ade6f6586f6a
Ben McMahan <benjamin@linkedresponder.com>
2026-06-27 21:23:59 -0400

    Stamp Sarah 0.1.54 build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.54",
-  "commit": "edce9f6",
-  "commitMessage": "Refresh packaged desktop build metadata",
-  "builtAt": "2026-06-28T01:18:53.470Z"
+  "commit": "e23db44",
+  "commitMessage": "Fix Profiler report evidence ranking",
+  "builtAt": "2026-06-28T01:22:57.491Z"
 };
