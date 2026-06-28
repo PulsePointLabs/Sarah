d6ab306fcaca1355ffa870597bcea441fb556a19
Ben McMahan <benjamin@linkedresponder.com>
2026-06-27 21:36:12 -0400

    Refresh Sarah 0.1.54 build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.54",
-  "commit": "e23db44",
-  "commitMessage": "Fix Profiler report evidence ranking",
-  "builtAt": "2026-06-28T01:22:57.491Z"
+  "commit": "c932b32",
+  "commitMessage": "Prevent reused image ID classification collisions",
+  "builtAt": "2026-06-28T01:35:14.563Z"
 };
