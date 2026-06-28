31fadd5ebab05d41fecb76a98de5b6f90ecc33e6
Ben McMahan <benjamin@linkedresponder.com>
2026-06-28 17:51:26 -0400

    Record Sarah 0.1.66 build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.66",
-  "commit": "6f51e39",
-  "commitMessage": "Queue Profiler reviews with saved image references",
-  "builtAt": "2026-06-28T21:48:15.980Z"
+  "commit": "200e04d",
+  "commitMessage": "Replace report TTS playback with chat engine behavior",
+  "builtAt": "2026-06-28T21:50:31.363Z"
 };
