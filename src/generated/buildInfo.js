edce9f6f5a1de92e665dd6418b4dcc6729e5e5ad
Ben McMahan <benjamin@linkedresponder.com>
2026-06-27 20:35:03 -0400

    Refresh packaged desktop build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.53",
-  "commit": "9cd5b2a",
-  "commitMessage": "Persist Profiler anatomy image classifications",
-  "builtAt": "2026-06-28T00:30:38.963Z"
+  "commit": "ce91291",
+  "commitMessage": "Stamp Sarah 0.1.53 build metadata",
+  "builtAt": "2026-06-28T00:32:37.452Z"
 };
