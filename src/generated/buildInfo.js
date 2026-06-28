ce91291572d8dc31ba17a7a4a3886a559b0e6046
Ben McMahan <benjamin@linkedresponder.com>
2026-06-27 20:31:17 -0400

    Stamp Sarah 0.1.53 build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.52",
-  "commit": "1790ee3",
-  "commitMessage": "Add native job status and media playback",
-  "builtAt": "2026-06-27T23:13:15.942Z"
+  "version": "0.1.53",
+  "commit": "9cd5b2a",
+  "commitMessage": "Persist Profiler anatomy image classifications",
+  "builtAt": "2026-06-28T00:30:38.963Z"
 };
