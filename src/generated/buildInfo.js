7df320eac5af64c72233960215fee3dce19da349
Ben McMahan <benjamin@linkedresponder.com>
2026-06-29 11:02:02 -0400

    Record background TTS export build

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.70",
-  "commit": "77ccc42",
-  "commitMessage": "Scope live blood pressure to active session",
-  "builtAt": "2026-06-29T07:44:15.733Z"
+  "commit": "a91d682",
+  "commitMessage": "Allow long background TTS exports",
+  "builtAt": "2026-06-29T15:00:49.661Z"
 };
