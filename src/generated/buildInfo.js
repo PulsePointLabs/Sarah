8d6cd1ad1ddc643cd4d40d6b7152c59c7e5ec7f0
Ben McMahan <benjamin@linkedresponder.com>
2026-06-29 00:47:20 -0400

    Refresh Sarah 0.1.69 build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.69",
-  "commit": "81c0b2a",
-  "commitMessage": "Stabilize session chat autosave on mobile",
-  "builtAt": "2026-06-29T04:45:28.647Z"
+  "commit": "530d214",
+  "commitMessage": "Align session review video with phase markers",
+  "builtAt": "2026-06-29T04:47:20.210Z"
 };
