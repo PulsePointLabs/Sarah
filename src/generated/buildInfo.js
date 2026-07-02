c6ce06356f2715d2710c90183f53813c07ff8aee
Ben McMahan <benjamin@linkedresponder.com>
2026-07-01 20:06:23 -0400

    Record profiler retry desktop build

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.79",
-  "commit": "7041ced",
-  "commitMessage": "Resume refused profiler batches from saved image metadata",
-  "builtAt": "2026-07-01T23:55:44.839Z"
+  "commit": "966f806",
+  "commitMessage": "Continue profiler synthesis after exhausted provider refusal",
+  "builtAt": "2026-07-01T23:58:19.880Z"
 };
