61a2371ebc4eae60dc5977202691fb6e75a4432b
Ben McMahan <benjamin@linkedresponder.com>
2026-07-03 18:18:46 -0400

    Record guarded narration build

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.81",
-  "commit": "0474ff1",
-  "commitMessage": "Reduce background TTS export requests",
-  "builtAt": "2026-07-03T21:17:00.687Z"
+  "commit": "d626376",
+  "commitMessage": "Reuse saved narration and cap OpenAI fanout",
+  "builtAt": "2026-07-03T22:16:25.140Z"
 };
