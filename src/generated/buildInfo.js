b9b2c19a71e7cf75e3904ae63bbeaf8ee583d2b5
Ben McMahan <benjamin@linkedresponder.com>
2026-07-04 17:32:40 -0400

    Record Sarah 0.1.82 build

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.81",
-  "commit": "d626376",
-  "commitMessage": "Reuse saved narration and cap OpenAI fanout",
-  "builtAt": "2026-07-03T22:16:25.140Z"
+  "version": "0.1.82",
+  "commit": "aad10c9",
+  "commitMessage": "Require sustained release for stimulation pauses",
+  "builtAt": "2026-07-04T21:28:59.193Z"
 };
