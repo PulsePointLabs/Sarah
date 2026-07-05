9be5d29c085fb9d6ec968302ea95bf942f1616dd
Ben McMahan <benjamin@linkedresponder.com>
2026-07-04 20:29:57 -0400

    Record repaired TTS export build

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.82",
-  "commit": "aad10c9",
-  "commitMessage": "Require sustained release for stimulation pauses",
-  "builtAt": "2026-07-04T21:28:59.193Z"
+  "commit": "2d6647a",
+  "commitMessage": "Retry broken TTS export chunks",
+  "builtAt": "2026-07-05T00:27:39.218Z"
 };
