e0c9b8e5a43ea37f58296835cbce6151466052fc
Ben McMahan <benjamin@linkedresponder.com>
2026-07-07 21:05:33 -0400

    Release Sarah 0.1.90

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.89",
-  "commit": "4d386fa",
-  "commitMessage": "Fix Android video sync fullscreen overlay and bump 0.1.89",
-  "builtAt": "2026-07-07T23:47:16.385Z"
+  "version": "0.1.90",
+  "commit": "aedc052",
+  "commitMessage": "Fix mobile video sync fullscreen overlay",
+  "builtAt": "2026-07-08T01:03:44.135Z"
 };
