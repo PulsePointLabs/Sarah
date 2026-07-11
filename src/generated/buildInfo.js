2e804ddae69f05d5182376961df317e915e58af7
Ben McMahan <benjamin@linkedresponder.com>
2026-07-10 23:10:36 -0400

    Release Sarah v0.1.101

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.100",
-  "commit": "e907668",
-  "commitMessage": "Release Sarah v0.1.100",
-  "builtAt": "2026-07-11T02:38:35.275Z"
+  "version": "0.1.101",
+  "commit": "96b79f6",
+  "commitMessage": "Stabilize profiler profile loading",
+  "builtAt": "2026-07-11T03:09:36.680Z"
 };
