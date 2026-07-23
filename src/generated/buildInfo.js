e0d3e4491db08c65d41d39492fde418a623d266e
Ben McMahan <benjamin@linkedresponder.com>
2026-07-23 18:11:55 -0400

    Record 0.1.146 build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.145",
-  "commit": "b7b3275",
-  "commitMessage": "Make live encouragement physiology adaptive",
-  "builtAt": "2026-07-23T21:13:11.807Z"
+  "version": "0.1.146",
+  "commit": "8952489",
+  "commitMessage": "Verify H10 raw sensors before capture",
+  "builtAt": "2026-07-23T22:10:44.640Z"
 };
