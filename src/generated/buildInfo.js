afb2909092ebd07c8abe28751034bef8c7d027f2
Ben McMahan <benjamin@linkedresponder.com>
2026-07-23 19:34:36 -0400

    Record 0.1.148 build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.147",
-  "commit": "fb97a3a",
-  "commitMessage": "Add synchronized physiology to Video Sync",
-  "builtAt": "2026-07-23T22:23:38.537Z"
+  "version": "0.1.148",
+  "commit": "c5aa8b8",
+  "commitMessage": "Fix H10 physiology source and cue previews",
+  "builtAt": "2026-07-23T23:33:59.461Z"
 };
