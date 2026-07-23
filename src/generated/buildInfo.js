86c09306a41da7c9a1f5eb98fe590e31b8631fd9
Ben McMahan <benjamin@linkedresponder.com>
2026-07-23 19:48:43 -0400

    Record 0.1.149 build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.148",
-  "commit": "c5aa8b8",
-  "commitMessage": "Fix H10 physiology source and cue previews",
-  "builtAt": "2026-07-23T23:33:59.461Z"
+  "version": "0.1.149",
+  "commit": "c909fdf",
+  "commitMessage": "Remove duplicate intimate cue option",
+  "builtAt": "2026-07-23T23:47:27.378Z"
 };
