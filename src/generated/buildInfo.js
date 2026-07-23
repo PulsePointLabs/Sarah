fba0175fab1b020404a6d301396b1dba97874981
Ben McMahan <benjamin@linkedresponder.com>
2026-07-23 18:24:56 -0400

    Record 0.1.147 build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.146",
-  "commit": "8952489",
-  "commitMessage": "Verify H10 raw sensors before capture",
-  "builtAt": "2026-07-23T22:10:44.640Z"
+  "version": "0.1.147",
+  "commit": "fb97a3a",
+  "commitMessage": "Add synchronized physiology to Video Sync",
+  "builtAt": "2026-07-23T22:23:38.537Z"
 };
