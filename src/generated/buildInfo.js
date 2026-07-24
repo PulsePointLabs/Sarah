10330fe554c5bb4d431cde7dad90f95309f06293
Ben McMahan <benjamin@linkedresponder.com>
2026-07-23 20:50:44 -0400

    Record 0.1.153 build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.152",
-  "commit": "e3a12d7",
-  "commitMessage": "Allow capture when optional H10 PMD fails",
-  "builtAt": "2026-07-24T00:28:48.236Z"
+  "version": "0.1.153",
+  "commit": "0d25e53",
+  "commitMessage": "Fix Polar H10 accelerometer PMD command",
+  "builtAt": "2026-07-24T00:50:06.041Z"
 };
