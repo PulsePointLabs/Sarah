d649e589867db368e60920be98d8b145202f4e02
Ben McMahan <benjamin@linkedresponder.com>
2026-07-24 12:59:37 -0400

    Record 0.1.162 final build

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.161",
-  "commit": "2f343f4",
-  "commitMessage": "Keep Android awake during active capture",
-  "builtAt": "2026-07-24T05:27:05.553Z"
+  "version": "0.1.162",
+  "commit": "a681fe6",
+  "commitMessage": "Add opt-in intimate session reflections",
+  "builtAt": "2026-07-24T16:58:17.051Z"
 };
