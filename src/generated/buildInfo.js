fd1b0d0c4129d688c5a5d4e7a427a144cacde5e2
Ben McMahan <benjamin@linkedresponder.com>
2026-07-24 01:28:19 -0400

    Record 0.1.161 final build

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.160",
-  "commit": "ea0838c",
-  "commitMessage": "Fix imperial height input feedback loop",
-  "builtAt": "2026-07-24T03:13:33.282Z"
+  "version": "0.1.161",
+  "commit": "2f343f4",
+  "commitMessage": "Keep Android awake during active capture",
+  "builtAt": "2026-07-24T05:27:05.553Z"
 };
