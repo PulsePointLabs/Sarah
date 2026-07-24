6bc4ac028bb1f77606b17ebe13acce777f889333
Ben McMahan <benjamin@linkedresponder.com>
2026-07-23 23:15:09 -0400

    Record 0.1.160 final build

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.159",
-  "commit": "f3f5728",
-  "commitMessage": "Use feet and inches for profile height",
-  "builtAt": "2026-07-24T03:05:25.739Z"
+  "version": "0.1.160",
+  "commit": "ea0838c",
+  "commitMessage": "Fix imperial height input feedback loop",
+  "builtAt": "2026-07-24T03:13:33.282Z"
 };
