99705503e1f5639ef711fe069efa3622bafcbb5e
Ben McMahan <benjamin@linkedresponder.com>
2026-07-23 20:00:26 -0400

    Record 0.1.150 build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.149",
-  "commit": "c909fdf",
-  "commitMessage": "Remove duplicate intimate cue option",
-  "builtAt": "2026-07-23T23:47:27.378Z"
+  "version": "0.1.150",
+  "commit": "01d28c8",
+  "commitMessage": "Add pulse oximetry to body exploration",
+  "builtAt": "2026-07-23T23:59:46.518Z"
 };
