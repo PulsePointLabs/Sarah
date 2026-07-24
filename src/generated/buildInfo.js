22f59bd21ed1ce331eb3f5d8fe7e6033bd710688
Ben McMahan <benjamin@linkedresponder.com>
2026-07-23 22:53:58 -0400

    Record 0.1.158 final build

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.157",
-  "commit": "33093b2",
-  "commitMessage": "Merge Health Connect composition records",
-  "builtAt": "2026-07-24T02:26:51.774Z"
+  "version": "0.1.158",
+  "commit": "8d6c3b7",
+  "commitMessage": "Add direct ESF-551 body composition capture",
+  "builtAt": "2026-07-24T02:52:21.119Z"
 };
