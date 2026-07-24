0cd87ef580357347ff761e1f139443a3610a9238
Ben McMahan <benjamin@linkedresponder.com>
2026-07-23 22:10:45 -0400

    Record 0.1.156 desktop build

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.156",
-  "commit": "58d139a",
-  "commitMessage": "Fix Health Connect body composition sync",
-  "builtAt": "2026-07-24T02:02:11.699Z"
+  "commit": "bfd1092",
+  "commitMessage": "Record 0.1.156 build metadata",
+  "builtAt": "2026-07-24T02:07:30.575Z"
 };
