4d602f6325d6e74b53929e93a65d1d70c1564fd0
Ben McMahan <benjamin@linkedresponder.com>
2026-07-23 22:29:04 -0400

    Record 0.1.157 final build

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.157",
-  "commit": "652dfa2",
-  "commitMessage": "Hide unavailable body composition metrics",
-  "builtAt": "2026-07-24T02:21:41.315Z"
+  "commit": "33093b2",
+  "commitMessage": "Merge Health Connect composition records",
+  "builtAt": "2026-07-24T02:26:51.774Z"
 };
