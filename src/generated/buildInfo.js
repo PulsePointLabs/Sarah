2ead5f2cc5eb3474d63880cb643b45ea05307949
Ben McMahan <benjamin@linkedresponder.com>
2026-07-23 21:33:31 -0400

    Record 0.1.155 build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.155",
-  "commit": "53742ec",
-  "commitMessage": "Record 0.1.154 build metadata",
-  "builtAt": "2026-07-24T01:30:01.665Z"
+  "commit": "8ce640d",
+  "commitMessage": "Add Health Connect body composition sync",
+  "builtAt": "2026-07-24T01:32:18.715Z"
 };
