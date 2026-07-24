379eef6b63b047f4fc4dab16efa6e0b8f390be13
Ben McMahan <benjamin@linkedresponder.com>
2026-07-23 23:07:09 -0400

    Record 0.1.159 final build

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.158",
-  "commit": "8d6c3b7",
-  "commitMessage": "Add direct ESF-551 body composition capture",
-  "builtAt": "2026-07-24T02:52:21.119Z"
+  "version": "0.1.159",
+  "commit": "f3f5728",
+  "commitMessage": "Use feet and inches for profile height",
+  "builtAt": "2026-07-24T03:05:25.739Z"
 };
