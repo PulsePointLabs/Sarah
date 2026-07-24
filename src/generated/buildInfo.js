c06ad2cebe4ca0215dd9cbc89caed698c48da380
Ben McMahan <benjamin@linkedresponder.com>
2026-07-23 20:29:25 -0400

    Record 0.1.152 build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.151",
-  "commit": "0c72219",
-  "commitMessage": "Add pulse ox import to body exploration",
-  "builtAt": "2026-07-24T00:15:10.731Z"
+  "version": "0.1.152",
+  "commit": "e3a12d7",
+  "commitMessage": "Allow capture when optional H10 PMD fails",
+  "builtAt": "2026-07-24T00:28:48.236Z"
 };
