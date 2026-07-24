60aff3aea8468821120b20cfa93ed7136b7c2412
Ben McMahan <benjamin@linkedresponder.com>
2026-07-23 20:15:45 -0400

    Record 0.1.151 build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.150",
-  "commit": "01d28c8",
-  "commitMessage": "Add pulse oximetry to body exploration",
-  "builtAt": "2026-07-23T23:59:46.518Z"
+  "version": "0.1.151",
+  "commit": "0c72219",
+  "commitMessage": "Add pulse ox import to body exploration",
+  "builtAt": "2026-07-24T00:15:10.731Z"
 };
