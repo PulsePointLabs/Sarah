53742ec7a68f8b301e4842fa9974e5ea5847140b
Ben McMahan <benjamin@linkedresponder.com>
2026-07-23 21:10:38 -0400

    Record 0.1.154 build metadata

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.153",
-  "commit": "0d25e53",
-  "commitMessage": "Fix Polar H10 accelerometer PMD command",
-  "builtAt": "2026-07-24T00:50:06.041Z"
+  "version": "0.1.154",
+  "commit": "01347b0",
+  "commitMessage": "Accept real-world EMAY pulse ox exports",
+  "builtAt": "2026-07-24T01:09:54.171Z"
 };
