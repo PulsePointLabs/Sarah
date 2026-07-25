4586f0dedf219e804dda7f66d59678c7498d32b9
Ben McMahan <benjamin@linkedresponder.com>
2026-07-25 02:33:04 -0400

    Record 0.1.166 final build

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.165",
-  "commit": "d4b8d96",
-  "commitMessage": "Align Sarah chat with intimate context",
-  "builtAt": "2026-07-25T02:24:11.206Z"
+  "version": "0.1.166",
+  "commit": "e350c00",
+  "commitMessage": "Prevent truncated Sarah chat replies",
+  "builtAt": "2026-07-25T06:32:13.917Z"
 };
