130df42e5daf8fd92db4f21b21626dee4bf2893f
Ben McMahan <benjamin@linkedresponder.com>
2026-07-25 15:06:19 -0400

    Record 0.1.167 final build

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.166",
-  "commit": "e350c00",
-  "commitMessage": "Prevent truncated Sarah chat replies",
-  "builtAt": "2026-07-25T06:32:13.917Z"
+  "version": "0.1.167",
+  "commit": "3c2eed9",
+  "commitMessage": "Make long-form Sarah chat timeout resilient",
+  "builtAt": "2026-07-25T19:04:58.571Z"
 };
