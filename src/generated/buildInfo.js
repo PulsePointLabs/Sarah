9f282b9a0b5a619102ffa30d1976b12153c0602c
Ben McMahan <benjamin@linkedresponder.com>
2026-07-24 21:44:17 -0400

    Record 0.1.164 final build

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.163",
-  "commit": "f9dda5b",
-  "commitMessage": "Strengthen erotic reflection voice",
-  "builtAt": "2026-07-25T01:18:52.765Z"
+  "version": "0.1.164",
+  "commit": "b544408",
+  "commitMessage": "Persist full-session intimate reflection settings",
+  "builtAt": "2026-07-25T01:42:39.947Z"
 };
