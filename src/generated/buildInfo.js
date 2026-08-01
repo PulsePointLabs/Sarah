443d589234d50a7b14606af77223c4ae819f1c4a
Ben McMahan <benjamin@linkedresponder.com>
2026-08-01 16:56:08 -0400

    Refresh build info for v0.1.191

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.167",
-  "commit": "deefc31",
-  "commitMessage": "Rescue Sarah conversation context seam",
-  "builtAt": "2026-08-01T20:54:34.279Z"
+  "version": "0.1.191",
+  "commit": "cd9d9cc",
+  "commitMessage": "Merge branch 'feature/sarah-conversation-context-rescue'",
+  "builtAt": "2026-08-01T20:56:08.386Z"
 };
