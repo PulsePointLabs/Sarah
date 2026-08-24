27c245eb9bbe64ce66e9ba30c50e01ebb985250d
Ben McMahan <benjamin@linkedresponder.com>
2026-08-24 15:11:42 -0400

    Refresh build info for v0.1.230

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.229",
-  "commit": "f65269b",
-  "commitMessage": "Fix telemetry theater layout and media switching",
-  "builtAt": "2026-08-24T19:01:13.938Z"
+  "version": "0.1.230",
+  "commit": "a4f33ee",
+  "commitMessage": "Bump Sarah to v0.1.230",
+  "builtAt": "2026-08-24T19:09:44.388Z"
 };
