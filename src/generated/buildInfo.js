acfa53f9c53f4c18c5681996be95cf1932f23b3b
Ben McMahan <benjamin@linkedresponder.com>
2026-08-24 15:47:18 -0400

    Refresh build info for v0.1.231

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.230",
-  "commit": "a4f33ee",
-  "commitMessage": "Bump Sarah to v0.1.230",
-  "builtAt": "2026-08-24T19:09:44.388Z"
+  "version": "0.1.231",
+  "commit": "4ecb057",
+  "commitMessage": "Bump Sarah to v0.1.231",
+  "builtAt": "2026-08-24T19:46:44.000Z"
 };
