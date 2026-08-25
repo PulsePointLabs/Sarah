09aec53172b274e1b252d411f8d096e3af63c8cf
Ben McMahan <benjamin@linkedresponder.com>
2026-08-25 19:56:29 -0400

    Refresh build info for v0.1.237

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.236",
-  "commit": "d62129e",
-  "commitMessage": "Make cloud analysis readable and reusable",
-  "builtAt": "2026-08-25T22:10:46.322Z"
+  "version": "0.1.237",
+  "commit": "2d90f1d",
+  "commitMessage": "Restore Sarah annotation-first cloud workflow",
+  "builtAt": "2026-08-25T23:56:26.106Z"
 };
