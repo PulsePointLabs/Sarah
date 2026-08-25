4d62aeaf581baa008537c7dfc31e01140f20f8fd
Ben McMahan <benjamin@linkedresponder.com>
2026-08-24 21:40:49 -0400

    Refresh build info for v0.1.234

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.233",
-  "commit": "ebc3371",
-  "commitMessage": "Refresh build info for v0.1.233",
-  "builtAt": "2026-08-25T00:24:22.535Z"
+  "version": "0.1.234",
+  "commit": "250ca3d",
+  "commitMessage": "Prevent review video overlay collisions",
+  "builtAt": "2026-08-25T01:40:49.503Z"
 };
