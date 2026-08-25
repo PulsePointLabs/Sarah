366620fbfce7b53f693279613198bf0cac28eaaf
Ben McMahan <benjamin@linkedresponder.com>
2026-08-24 20:39:41 -0400

    Record v0.1.233 packaged build provenance

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.233",
-  "commit": "eb0c5ec",
-  "commitMessage": "Fix review video timing and telemetry theater",
-  "builtAt": "2026-08-25T00:21:09.986Z"
+  "commit": "ebc3371",
+  "commitMessage": "Refresh build info for v0.1.233",
+  "builtAt": "2026-08-25T00:24:22.535Z"
 };
