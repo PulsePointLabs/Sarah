1ef32a49b3d5113ef39482b1919220d61da24162
Ben McMahan <benjamin@linkedresponder.com>
2026-08-24 21:47:03 -0400

    Record v0.1.234 packaged build provenance

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.234",
-  "commit": "250ca3d",
-  "commitMessage": "Prevent review video overlay collisions",
-  "builtAt": "2026-08-25T01:40:49.503Z"
+  "commit": "4d62aea",
+  "commitMessage": "Refresh build info for v0.1.234",
+  "builtAt": "2026-08-25T01:45:59.760Z"
 };
