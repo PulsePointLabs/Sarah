35128670b983e45ce9368c8739cdef7b0baf4833
Ben McMahan <benjamin@linkedresponder.com>
2026-08-25 16:53:58 -0400

    Refresh build info for v0.1.235

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.234",
-  "commit": "4d62aea",
-  "commitMessage": "Refresh build info for v0.1.234",
-  "builtAt": "2026-08-25T01:45:59.760Z"
+  "version": "0.1.235",
+  "commit": "517b4b2",
+  "commitMessage": "Persist cloud analysis results to session records",
+  "builtAt": "2026-08-25T20:53:16.461Z"
 };
