eab031c4cbb70f270b7064a171c80ad54338bb09
Ben McMahan <benjamin@linkedresponder.com>
2026-08-26 15:38:11 -0400

    Refresh build info for v0.1.239

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.238",
-  "commit": "ddfaf1d",
-  "commitMessage": "Keep cloud evidence behind Sarah annotation UI",
-  "builtAt": "2026-08-26T00:21:55.754Z"
+  "version": "0.1.239",
+  "commit": "36726f0",
+  "commitMessage": "Integrate cloud evidence into Sarah analysis",
+  "builtAt": "2026-08-26T19:38:11.597Z"
 };
