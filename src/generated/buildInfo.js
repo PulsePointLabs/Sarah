b79166347e1a06efd107f69e918c47c393a2214b
Ben McMahan <benjamin@linkedresponder.com>
2026-08-26 16:26:38 -0400

    Record packaged v0.1.240 build provenance

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.239",
-  "commit": "eab031c",
-  "commitMessage": "Refresh build info for v0.1.239",
-  "builtAt": "2026-08-26T19:39:31.234Z"
+  "version": "0.1.240",
+  "commit": "c14ca4a",
+  "commitMessage": "Make Live Capture wait for OBS and quiet setup cues",
+  "builtAt": "2026-08-26T20:26:21.288Z"
 };
