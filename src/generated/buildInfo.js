48ca769619e27979891040bca7f22bbe95a5fd26
Ben McMahan <benjamin@linkedresponder.com>
2026-08-26 22:43:25 -0400

    Record packaged v0.1.244 build provenance

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.243",
-  "commit": "8d30bb6",
-  "commitMessage": "Brand Windows app as Sarah",
-  "builtAt": "2026-08-27T02:19:19.420Z"
+  "version": "0.1.244",
+  "commit": "9cdef96",
+  "commitMessage": "Require context for near-climax confirmation",
+  "builtAt": "2026-08-27T02:42:22.676Z"
 };
