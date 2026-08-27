b39726523d9da05f4bc3fe8cc7f8462ee907a629
Ben McMahan <benjamin@linkedresponder.com>
2026-08-27 17:11:20 -0400

    Record packaged v0.1.247 build provenance

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.246",
-  "commit": "9a79bc8",
-  "commitMessage": "Upgrade visual findings lightbox",
-  "builtAt": "2026-08-27T20:20:37.211Z"
+  "version": "0.1.247",
+  "commit": "1eb2177",
+  "commitMessage": "Restore saved visual evidence in Sarah chat",
+  "builtAt": "2026-08-27T21:08:44.267Z"
 };
