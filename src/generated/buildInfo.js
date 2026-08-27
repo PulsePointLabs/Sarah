14d26195017da5cc84cc89850a9e0c386f69dc68
Ben McMahan <benjamin@linkedresponder.com>
2026-08-26 22:23:58 -0400

    Record branded Windows build provenance

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.243",
-  "commit": "d554056",
-  "commitMessage": "Add confirmed near-climax timeline overlays",
-  "builtAt": "2026-08-27T02:08:44.253Z"
+  "commit": "8d30bb6",
+  "commitMessage": "Brand Windows app as Sarah",
+  "builtAt": "2026-08-27T02:19:19.420Z"
 };
