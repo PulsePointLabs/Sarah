4c46f68ffd1a4dad21bab6e0de5931f5ce2c9772
Ben McMahan <benjamin@linkedresponder.com>
2026-08-28 16:59:43 -0400

    Record packaged v0.1.249 build provenance

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.248",
-  "commit": "db2c372",
-  "commitMessage": "Optimize Sarah release pipeline",
-  "builtAt": "2026-08-28T20:43:20.154Z"
+  "version": "0.1.249",
+  "commit": "23e37d3",
+  "commitMessage": "Release Sarah v0.1.249",
+  "builtAt": "2026-08-28T20:59:12.960Z"
 };
