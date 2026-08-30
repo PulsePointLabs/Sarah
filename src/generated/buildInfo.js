e6a10688fe3cfe72a6d7cf71151bed1544527a90
Ben McMahan <benjamin@linkedresponder.com>
2026-08-30 17:06:39 -0400

    Record packaged v0.1.250 build provenance

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.249",
-  "commit": "0fe4f3f",
-  "commitMessage": "Restore repeated near-climax episodes",
-  "builtAt": "2026-08-30T21:04:06.813Z"
+  "version": "0.1.250",
+  "commit": "024e9bf",
+  "commitMessage": "Release Sarah v0.1.250",
+  "builtAt": "2026-08-30T21:06:05.257Z"
 };
