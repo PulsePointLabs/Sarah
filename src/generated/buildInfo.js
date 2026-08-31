bcc2a1df941883c79defbc92fbb45e53367da451
Ben McMahan <benjamin@linkedresponder.com>
2026-08-30 20:02:55 -0400

    Record packaged v0.1.253 build provenance

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.252",
-  "commit": "5c41497",
-  "commitMessage": "Fix deep-route browser refreshes in v0.1.252",
-  "builtAt": "2026-08-30T23:08:10.115Z"
+  "version": "0.1.253",
+  "commit": "d9f3bdc",
+  "commitMessage": "Retry transient TTS export failures in v0.1.253",
+  "builtAt": "2026-08-31T00:02:01.048Z"
 };
