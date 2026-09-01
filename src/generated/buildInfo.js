3c3d58d52576417f6798120c7619d7426f71c335
Ben McMahan <benjamin@linkedresponder.com>
2026-09-01 17:23:15 -0400

    Record media retention build provenance

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.255",
-  "commit": "4bbea70",
-  "commitMessage": "Release Sarah v0.1.255",
-  "builtAt": "2026-09-01T20:52:16.181Z"
+  "commit": "fbd2582",
+  "commitMessage": "Bound Sarah media cache growth",
+  "builtAt": "2026-09-01T21:22:57.188Z"
 };
