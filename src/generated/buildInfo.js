dd1b2e367869c92f3b7103ec60da37ab9cbd8d29
Ben McMahan <benjamin@linkedresponder.com>
2026-09-01 16:54:59 -0400

    Record packaged v0.1.255 build provenance

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.254",
-  "commit": "a27f98b",
-  "commitMessage": "Fix live detector timing and desktop vitals",
-  "builtAt": "2026-09-01T20:40:22.534Z"
+  "version": "0.1.255",
+  "commit": "4bbea70",
+  "commitMessage": "Release Sarah v0.1.255",
+  "builtAt": "2026-09-01T20:52:16.181Z"
 };
