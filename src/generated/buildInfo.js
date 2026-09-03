e2d7dfb47e95638fe6b26d8c3be77a75fcdf9a79
Ben McMahan <benjamin@linkedresponder.com>
2026-09-03 14:57:10 -0400

    Record exploration evidence build provenance

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.255",
-  "commit": "6210dc5",
-  "commitMessage": "Prioritize body response in exploration analysis",
-  "builtAt": "2026-09-03T18:44:31.978Z"
+  "commit": "6704035",
+  "commitMessage": "Require physiology before exploration analysis",
+  "builtAt": "2026-09-03T18:56:27.407Z"
 };
