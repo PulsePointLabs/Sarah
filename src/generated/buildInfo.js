84e6b24278cef88e5dbd096b5b0d4a968e1461d6
Ben McMahan <benjamin@linkedresponder.com>
2026-09-03 14:45:22 -0400

    Record exploration synthesis build provenance

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.255",
-  "commit": "fbd2582",
-  "commitMessage": "Bound Sarah media cache growth",
-  "builtAt": "2026-09-01T21:22:57.188Z"
+  "commit": "6210dc5",
+  "commitMessage": "Prioritize body response in exploration analysis",
+  "builtAt": "2026-09-03T18:44:31.978Z"
 };
