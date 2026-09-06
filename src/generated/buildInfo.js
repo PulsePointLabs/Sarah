6b9a51348ebef8a6c0cb31a22539919c096ad1c7
Ben McMahan <benjamin@linkedresponder.com>
2026-09-06 14:42:35 -0400

    Record Sarah 0.1.261 build provenance

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.260",
-  "commit": "c746dee",
-  "commitMessage": "Fix Sarah narration confusing vital ranges with elapsed time",
-  "builtAt": "2026-09-06T01:35:23.861Z"
+  "version": "0.1.261",
+  "commit": "b4eb3b6",
+  "commitMessage": "Fix live encouragement audio readiness and restore visible volume controls",
+  "builtAt": "2026-09-06T18:39:40.186Z"
 };
