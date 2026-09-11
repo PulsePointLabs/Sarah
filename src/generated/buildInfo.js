0589fcec453331f4dabc66ab640d9fdd7c498387
Ben McMahan <benjamin@linkedresponder.com>
2026-09-11 18:49:59 -0400

    Record phase overlay and subjective episodes frontend build

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.263",
-  "commit": "f5e0292",
-  "commitMessage": "Add F focus view and bounded resizable telemetry sidebar",
-  "builtAt": "2026-09-11T04:59:40.309Z"
+  "commit": "1c7a7b2",
+  "commitMessage": "Add N and C subjective episode markers with telemetry and duration summaries",
+  "builtAt": "2026-09-11T22:49:13.907Z"
 };
