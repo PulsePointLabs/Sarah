819f9052a2e6bfb689e019b985cdbd95ccbf1ce6
Ben McMahan <benjamin@linkedresponder.com>
2026-09-11 19:28:18 -0400

    Record Session Details dashboard overlay build

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.263",
-  "commit": "6639802",
-  "commitMessage": "Add slim contextual physiology graphs to subjective episode cards",
-  "builtAt": "2026-09-11T23:09:31.023Z"
+  "commit": "17722f0",
+  "commitMessage": "Wire phase and subjective episode toggles into Session Snapshot HR dashboard",
+  "builtAt": "2026-09-11T23:27:47.707Z"
 };
