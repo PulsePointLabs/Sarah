2fa0b7fc4e7f767c6028b0d2f4548b5c258dd108
Ben McMahan <benjamin@linkedresponder.com>
2026-09-12 19:43:01 -0400

    Update build metadata for Howl recording and timelines

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.263",
-  "commit": "a786933",
-  "commitMessage": "Prevent partial telemetry packets resetting physiological load",
-  "builtAt": "2026-09-12T01:17:04.409Z"
+  "commit": "ab30422",
+  "commitMessage": "Record external Howl settings and add synchronized review timelines",
+  "builtAt": "2026-09-12T23:42:04.106Z"
 };
