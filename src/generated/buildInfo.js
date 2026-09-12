9c6cce2516447d059e0df9ec5ff4376a36ffba16
Ben McMahan <benjamin@linkedresponder.com>
2026-09-11 21:17:28 -0400

    Update build metadata for load packet fix

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
   "version": "0.1.263",
-  "commit": "aba7ce1",
-  "commitMessage": "Show physiological load for Body Exploration full telemetry",
-  "builtAt": "2026-09-12T01:12:18.111Z"
+  "commit": "a786933",
+  "commitMessage": "Prevent partial telemetry packets resetting physiological load",
+  "builtAt": "2026-09-12T01:17:04.409Z"
 };
