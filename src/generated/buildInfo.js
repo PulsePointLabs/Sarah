a626a59008d7245780e5a17d28a52421da8031b5
Ben McMahan <benjamin@linkedresponder.com>
2026-09-23 17:50:02 -0400

    Stamp Sarah 0.1.264 background capture build

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = {
   "appName": "Sarah",
-  "version": "0.1.263",
-  "commit": "3f80a72",
-  "commitMessage": "Match Haruna bracket playback speed shortcuts in Video Sync",
-  "builtAt": "2026-09-23T00:56:07.913Z"
+  "version": "0.1.264",
+  "commit": "f4d6d6a",
+  "commitMessage": "Keep Android H10 capture native while other apps are foregrounded",
+  "builtAt": "2026-09-23T21:48:59.450Z"
 };
